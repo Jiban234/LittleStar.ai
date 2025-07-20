169afe293f85bb833fdd99f168e39106af8c8772
Jiban Jyoti Das <jibanjyotidas016@gmail.com>
2025-07-20 20:20:20 +0530

    My Second Commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/Aitools'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Hero/>
+      <AiTools/>
+      <Testimonial></Testimonial>
+    </> 
   )
 }
 
